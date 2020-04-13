634400c5d62da7139cc3f1500618a26acdf78d24
sgraton <steven.graton@gmail.com>
2020-04-13 19:04:31 +0200

    Offers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_13_085444) do
+ActiveRecord::Schema.define(version: 2020_04_13_165409) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -66,6 +66,19 @@ ActiveRecord::Schema.define(version: 2020_04_13_085444) do
     t.index ["user_id"], name: "index_gigs_on_user_id"
   end
 
+  create_table "offers", force: :cascade do |t|
+    t.text "note"
+    t.integer "amount"
+    t.integer "days"
+    t.integer "status", default: 0
+    t.bigint "request_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["request_id"], name: "index_offers_on_request_id"
+    t.index ["user_id"], name: "index_offers_on_user_id"
+  end
+
   create_table "orders", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.date "due_date"
     t.string "title"
@@ -78,8 +91,10 @@ ActiveRecord::Schema.define(version: 2020_04_13_085444) do
     t.bigint "seller_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "request_id"
     t.index ["buyer_id"], name: "index_orders_on_buyer_id"
     t.index ["gig_id"], name: "index_orders_on_gig_id"
+    t.index ["request_id"], name: "index_orders_on_request_id"
     t.index ["seller_id"], name: "index_orders_on_seller_id"
   end
 
@@ -131,7 +146,10 @@ ActiveRecord::Schema.define(version: 2020_04_13_085444) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "gigs", "categories"
   add_foreign_key "gigs", "users"
+  add_foreign_key "offers", "requests"
+  add_foreign_key "offers", "users"
   add_foreign_key "orders", "gigs"
+  add_foreign_key "orders", "requests"
   add_foreign_key "orders", "users", column: "buyer_id"
   add_foreign_key "orders", "users", column: "seller_id"
   add_foreign_key "pricings", "gigs"
