b678543c85640007faf859fd2e07d3b50828b6c8
sgraton <steven.graton@gmail.com>
2021-05-11 13:06:41 +0200

    Add user test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should not save user without full_name" do
+    user = User.new
+    assert_not user.save
+  end
+
+  test "is offline" do
+    user = User.new
+    user.last_seen = nil
+    assert_not user.online?
+
+    user.last_seen =  Time.now - 11.minutes
+    assert_not user.online?
+  end
+
+  test "is online" do
+    user = User.new
+    user.last_seen = Time.now
+    assert user.online?
+
+    user.last_seen =  Time.now - 5.minutes
+    assert user.online?
+
+    user.last_seen = Time.now - 9.minutes - 59.seconds
+    assert user.online?
+  end
 end
