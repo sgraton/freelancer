496794018a7f0990c38bbb440b376bd795c241b1
sgraton <steven.graton@gmail.com>
2021-05-12 09:04:43 +0200

    Add transaction test

--- test/models/transaction_test.rb
+++ test/models/transaction_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class TransactionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create with nothing" do
+    transaction = Transaction.new
+    assert transaction.save
+  end
 end
